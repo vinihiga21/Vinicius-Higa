6897dcb9e20d646b5344518557e7f9c27e802bf6
vinihiga21 <167332326+vinihiga21@users.noreply.github.com>
2024-09-09 08:42:11 -0300

    calculadora

---
+++ exercicios/script.js
@@ -0,0 +1,10 @@
+function calcular() {
+    let number = parseInt(document.getElementById('number').value);
+    let number2 = parseInt(document.getElementById('number2').value);
+    let number3 = parseInt(document.getElementById('number3').value);
+    let number4 = parseInt(document.getElementById('number4').value);
+
+    console.log(math.min(number, number2, number3, number4))
+    console.log(math.max(number, number2, number3, number4))
+    console.log(math.floor)
+}
